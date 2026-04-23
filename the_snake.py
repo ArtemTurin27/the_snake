d051bb5e9d22e51f9cb945674dec569cdaa7dbaa
Roman <fnajt08@gmail.com>
2026-04-23 11:47:44 +0300

    Добавленные классы GameObject и Apple

--- the_snake.py
+++ the_snake.py
@@ -40,7 +40,63 @@ clock = pygame.time.Clock()
 
 
 # Тут опишите все классы игры.
-...
+class GameObjact:
+    """"
+    Базовый класс, от которого наследуется все обьекты,
+    Содержит общие атрибуты: позиция и цвет.  
+     """
+    
+    def __init__(self, position=None, body_color=None)
+        """
+        Конструкотор базового игрового обьекта.
+        Аргументы: position (координаты), body_color (цвет)
+        """
+        if position is None:
+            self.position = (320, 240)
+        else:
+            self.position  = position
+
+    def draw(self, surfese):
+        """
+        Абстрактный метод для отрисовки обьекта на экране.
+        Аргумент: surfase (поверхность, на которой рисуем)
+        """
+        pass 
+
+class Apple(GameObject)
+    """
+    Класс Apple. Наследуется от GameObject.
+    появляется в случайном месте поля.
+    """
+    apple_collor = (255, 0, 0)
+
+    super().__init__(position=None, body_color=apple_collor)
+
+    self.randomize_position()
+
+    def randomize_position(self):
+        """
+        Устанавливает случайные координаты для яблока
+        """
+        max_x = 640 - 20
+        max_y = 480 - 20
+
+        x.random.randrage(0, max_x + 1, 20)
+        y.random.randrage(0, max_y + 1, 20)
+
+        self.position = (x, y)
+
+        def draw(self, surfase):
+            """
+            Отрисовует яблоко на игровом поле
+            """
+
+            rect = pygame.Rect(
+                self.position[0]
+                self.position 1
+                20
+                20.
+            )
 
 
 def main():
